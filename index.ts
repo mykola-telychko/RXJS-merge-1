36a773e4ec88bb225ce070fdf34515f47a177eca
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-08 06:00:08 +0300

    fin

--- index.ts
+++ index.ts
@@ -1,21 +1,23 @@
 import { mapTo } from 'rxjs/operators';
 import { interval, merge } from 'rxjs';
 
+// src: https://www.learnrxjs.io/learn-rxjs/operators/combination/merge
+
 //emit every 2.5 seconds
-const first = interval(3500);
-//emit every 2 seconds
-const second = interval(3000);
-//emit every 1.5 seconds
-const third = interval(2500);
-//emit every 1 second
-const fourth = interval(2000);
+const delemiter = interval(6000);
+const first = interval(4500);
+const second = interval(4000);
+const third = interval(3500);
+const fourth = interval(3000);
 
 //emit outputs from one observable
+// merge all observ.pipe
 const example = merge(
+  delemiter.pipe(mapTo('<--000000000000000000-->')),
   first.pipe(mapTo('1-FIRST!')),
-  second.pipe(mapTo('2-SECOND!')),
-  third.pipe(mapTo('3-THIRD')),
-  fourth.pipe(mapTo('4-FOURTH'))
+  second.pipe(mapTo('2--SECOND!')),
+  third.pipe(mapTo('3---THIRD')),
+  fourth.pipe(mapTo('4----FOURTH'))
 );
-//output: "FOURTH", "THIRD", "SECOND!", "FOURTH", "FIRST!", "THIRD", "FOURTH"
+
 const subscribe = example.subscribe((val) => console.log(val));
